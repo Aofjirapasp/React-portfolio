829a90a13991fcd140fc2bfd03a8fcb0333fe4ea
SoftwareDevJpark <jparkingdeveloper@gmail.com>
2022-07-22 13:51:04 +0700

    update logo

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-// import profilepic from '../img/profile.jpg';
+// import profilepic from '../img/aof.jpg';
+import logo from '../img/aof.jpg'; // with import
 import Social from '../components/Social';
 import ReactTypingEffect from 'react-typing-effect'
 
@@ -7,7 +8,7 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-                <img src="../img/profile.jpg" alt="profile" className="profilepic" />
+                <img src={logo} alt="profile" className="profilepic" />
                 <ReactTypingEffect text={['I am Jirapas', 'I am a Frontend Developer']} speed={80} eraseDelay={200} className="typingeffect" />
                 <Social />
             </div>
